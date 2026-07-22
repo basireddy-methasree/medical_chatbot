7dc85a83d71b5934f5271b4f9ce14b5a1377ce57
basireddy-methasree <basireddymethasree2004@gmail.com>
2026-07-22 13:42:06 +0530

    Use langchain-huggingface embeddings

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.documents import Document
 from typing import List
 
